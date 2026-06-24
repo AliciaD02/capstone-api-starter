8d1940d9a3cddfd756342629e3c5352944f0f41b
Alicia Diaz <diazalicia4542@gmail.com>
2026-06-24 13:57:46 -0500

    added a method that updates the profile of the logged-in user then it finds the user's id and saves the updated profile.

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -2,10 +2,7 @@ package org.yearup.controllers;
 
 // created this ProfileController to receive requests and communicated with the profileService
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.models.Profile;
 import org.yearup.service.ProfileService;
 import org.yearup.service.UserService;
@@ -22,16 +19,14 @@ public class ProfileController {
     private final ProfileService profileService;
     private final UserService userService;
 
-    public ProfileController(ProfileService profileService, UserService userService)
-    {
+    public ProfileController(ProfileService profileService, UserService userService) {
         this.profileService = profileService;
         this.userService = userService;
     }
 
     // gets the profile of hte user who is currently logged in
     @GetMapping
-    public Profile getProfile(Principal principal)
-    {
+    public Profile getProfile(Principal principal) {
         //Gets the logged in user's username
         String username = principal.getName();
         // then uses the username to find the user's id
@@ -40,5 +35,16 @@ public class ProfileController {
         return profileService.getById(userId);
     }
 
+    // Updates the profile of the logged in user.
+    @PutMapping
+    public Profile updateProfile(@RequestBody Profile profile, Principal principal) {
+        // Gets the logged in user's username.
+        String username = principal.getName();
 
+        // Uses the username to find the user's id.
+        int userId = userService.getIdByUsername(username);
+
+        // Updates the profile and saves the changes.
+        return profileService.update(userId, profile);
+    }
 }
